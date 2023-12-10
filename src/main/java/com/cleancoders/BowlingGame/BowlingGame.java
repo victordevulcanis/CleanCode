a7965f526a85967fd81cbc03e07ecfd76f41199b
Victor <victordevulcanis@gmail.com>
2023-12-10 20:18:28 +0100

    Initial commit

---
+++ src/main/java/com/cleancoders/BowlingGame/BowlingGame.java
@@ -0,0 +1,51 @@
+package com.cleancoders.BowlingGame;
+
+public class BowlingGame {
+
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pins) {
+        rolls[currentRoll++] = pins;
+    }
+
+    public int score() {
+        int score = 0;
+        int frameIndex = 0;
+
+        for (int frame = 0; frame < 10; frame++)
+            if (isStrike(frameIndex)) {
+                score += 10 + nextTwoRollsAfterStrike(frameIndex);
+                frameIndex++;
+            } else if (isSpare(frameIndex)) {
+                score += 10 + nextRollAfterSpare(frameIndex);
+                frameIndex += 2;
+            } else {
+                score += twoRollsInFrame(frameIndex);
+                frameIndex += 2;
+            }
+        return score;
+    }
+
+    private int nextTwoRollsAfterStrike(int frameIndex){
+        return  rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    }
+
+    private int nextRollAfterSpare(int frameIndex){
+        return rolls[frameIndex + 2];
+    }
+
+    private int twoRollsInFrame(int frameIndex){
+        return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+    }
+
+
+}
---
+++ src/test/java/com/cleancoders/BowlingGame/BowlingTest.java
@@ -0,0 +1,66 @@
+package com.cleancoders.BowlingGame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BowlingTest {
+
+    private BowlingGame g = new BowlingGame();
+
+    private void rollMany(int numberOfFrames, int numberOfPins) {
+        for (int i = 0; i < numberOfFrames; i++) {
+            g.roll(numberOfPins);
+        }
+    }
+
+    private void rollSpare() {
+        g.roll(5);
+        g.roll(5);
+    }
+
+    private void rollStrike() {
+        g.roll(10);
+    }
+
+    @Test
+    public void canRoll() {
+        g.roll(0);
+    }
+
+    @Test
+    public void gutterGame() {
+        rollMany(20, 0);
+        assertEquals(0, g.score());
+    }
+
+    @Test
+    public void allOnes() {
+        rollMany(20, 1);
+        assertEquals(20, g.score());
+    }
+
+    @Test
+    public void oneSpare() {
+        rollSpare();
+        g.roll(3);
+        rollMany(17, 0);
+        assertEquals(16, g.score());
+    }
+
+    @Test
+    public void oneStrike() {
+        rollStrike();
+        g.roll(3);
+        g.roll(4);
+        rollMany(16, 0);
+        assertEquals(24, g.score());
+    }
+
+    @Test
+    public void perfectGame() {
+        rollMany(12, 10);
+        assertEquals(300, g.score());
+    }
+
+}
